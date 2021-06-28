2c6923d5ab7dd75a931a8abed1208ecb7b4177b2
Alex Washburn <github@recursion.ninja>
2021-06-28 15:49:06 -0400

    Combining Brooklyn-- and Manhattan-bound trains

--- results.sh
+++ results.sh
@@ -22,19 +22,19 @@ iterations=1000
 #done
 
 # Simulate station limiting
-#for d in Brooklyn Manhattan;
-#do
-#    for q in 50 60 70 80 90 100 110 120 130 140 150;
-#    do
-#         python3 subway_virus_simulation.py queue=$q $iterations $d schedules/$d-Bound+X.txt L-Subway-Line.csv info no-plot quiet=true $logfile
-#    done
-#done         
-
-# Simulate different train schedules
 for d in Brooklyn Manhattan;
 do
-    for h in X 0 1 2 3 4 5;
+    for q in 50 60 70 80 90 100 110 120 130 140 150;
     do
-         python3 subway_virus_simulation.py $iterations $d schedules/$d-Bound+$h.txt L-Subway-Line.csv info no-plot quiet=true $logfile
+         python3 subway_virus_simulation.py queue=$q $iterations $d schedules/$d-Bound+X.txt L-Subway-Line.csv info no-plot quiet=true $logfile
     done
-done 
+done         
+
+# Simulate different train schedules
+#for d in Brooklyn Manhattan;
+#do
+#    for h in X 0 1 2 3 4 5;
+#    do
+#         python3 subway_virus_simulation.py $iterations $d schedules/$d-Bound+$h.txt L-Subway-Line.csv info no-plot quiet=true $logfile
+#    done
+#done 
